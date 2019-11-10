9c1d4efc24772b391223251cb2ea02c46271b71a
Ricardo Bordería Pi <rychyy999@gmail.com>
2019-11-10 21:18:05 +0100

    Added envío a varios contactos

---
+++ EnviarWhatsapp/EnviarWhatsAppVariosTarget.py
@@ -0,0 +1,46 @@
+from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.keys import Keys
+from time import sleep
+
+# se debe tener instalado selenium
+# si no, introduce esto en la terminal: pip install selenium
+
+# to chromedriver in your computer
+driver = webdriver.Chrome("../chromedriver.exe")
+
+# abrir whstsapp web
+driver.get("https://web.whatsapp.com/")
+wait = WebDriverWait(driver, 600)
+
+# Nombre del contacto o grupo
+target = []
+contacto = ""
+while contacto != "fin":
+    contacto = input("Introduce el nombre del contacto o grupo: ")
+    if contacto != "fin":
+        target.append(contacto)
+
+# mensaje a enviar
+string = input("Introduce el mensaje: ")
+
+#número de veces que se va a repetir el mensaje
+numVeces = int(input("Introduce el número de veces que se repetir el mensaje: "))
+
+# para seleccionar el contacto o grupo
+for i in target:
+    x_arg = '//span[@title = "{}"]'.format(i)
+    group_title = driver.find_element_by_xpath(x_arg) 
+    group_title.click()
+    for i in range(numVeces):
+        # seleccionar el campo de texto
+        inp_xpath = '//*[@id="main"]/footer/div[1]/div[2]/div/div[2]'
+        input_box = driver.find_element_by_xpath(inp_xpath)
+        # introduce el mensaje en el campo de texto
+        input_box.send_keys(string)
+        # pulsa enter y envía el mensaje
+        input_box.send_keys(Keys.ENTER)
+        # esperar antes de enviar el siguiente mensaje
+        sleep(1)
+    
+driver.close()
